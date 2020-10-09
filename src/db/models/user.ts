8bffb946e66165c1681aa722df652cc412054e4d
MRsabs <mdiq287@gmail.com>
2020-10-09 22:18:12 +0300

    added user model

---
+++ src/db/models/user.ts
@@ -0,0 +1,39 @@
+import { mongoClient } from '@src/plugins/mongoClient';
+import { Document, Schema } from 'mongoose';
+import { nanoid } from 'nanoid';
+
+const UserSchema: Schema = new Schema({
+	_id: {
+		type: String,
+		default: () => nanoid(),
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	avatar: {
+		type: String,
+		default: null,
+	},
+});
+
+const USER = mongoClient.model<IUser>('User', UserSchema);
+
+export default USER;
+
+export interface IUser extends Document {
+	email: string;
+	firstName: string;
+	lastName: string;
+	avatar?: string;
+}
