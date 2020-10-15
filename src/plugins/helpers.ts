76cb2bf0a2dc3985eabb179aafa95132726b450a
MRsabs <mdiq287@gmail.com>
2020-10-15 23:00:19 +0300

    added a very common funcs as plugin

---
+++ src/plugins/helpers.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import fp from 'fastify-plugin';
+
+class Helpers {
+  async isAuthenticated(request: FastifyRequest, reply: FastifyReply) {
+    if (!request.session.authenticated) {
+      reply.status(401).send({ msg: 'unauthenticated' });
+      return;
+    } else {
+      return;
+    }
+  }
+}
+
+export default fp(
+  async function (fastify: FastifyInstance) {
+    fastify.decorate('helpers', new Helpers());
+  },
+  { name: 'helpers' }
+);
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    helpers: Helpers;
+  }
+}
