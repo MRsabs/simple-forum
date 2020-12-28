8a61b7c960b617b2aadb25f62f7daf269baad2ec
MRsabs <mdiq287@gmail.com>
2020-12-28 12:50:19 +0300

    add route to get user info

---
+++ src/routes/user/getUser.ts
@@ -0,0 +1,26 @@
+import USER from '@src/db/models/user';
+import { FastifyInstance } from 'fastify';
+
+export default async function (fastify: FastifyInstance): Promise<void> {
+  fastify.get('/:slug', async (request, reply) => {
+    try {
+      const { slug } = request.params as RequestBody;
+      const user = await USER.findOne({ slug });
+      if (!user) {
+        return reply.status(404).send({ msg: 'Not found' });
+      } else {
+        const userData = user.toJSON();
+        delete userData.__v;
+        delete userData.password;
+        return reply.status(200).send(userData);
+      }
+    } catch (error) {
+      fastify.log.error(error);
+      return reply.status(500).send({ msg: 'Something went wrong' });
+    }
+  });
+}
+
+interface RequestBody {
+  slug: string;
+}
