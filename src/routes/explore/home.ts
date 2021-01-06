283cc021f72f2d45ddbf95ca63b37c1559668c6e
MRsabs <mdiq287@gmail.com>
2021-01-06 16:24:50 +0300

    fix types

--- src/routes/explore/home.ts
+++ src/routes/explore/home.ts
@@ -1,25 +1,25 @@
-// @ts-nocheck
-import POST from '@src/db/models/post';
 import { FastifyInstance } from 'fastify';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
-  fastify.get('/', async function (request, reply) {
+  fastify.get<{ Querystring: { load?: number } }>('/', async function (request, reply) {
     try {
+      const res: unknown[] = [];
       const load = request.query.load || 0;
-      const posts = await POST.find()
+      const posts = await fastify.mongoClientModels.Post.find()
         .skip(load * 4)
         .limit(4)
         .sort({ createdAt: -1 });
       for (let i = 0; i < posts.length; i++) {
-        await posts[i].populate('author').execPopulate();
-        posts[i] = posts[i].toObject();
-        delete posts[i].__v;
-        delete posts[i].author.__v;
-        delete posts[i].author._id;
-        delete posts[i].author.email;
-        delete posts[i].author.password;
+        let post = posts[i];
+        await post.populate('author').execPopulate();
+        post = post.toJSON();
+        delete post.__v;
+        delete post._id;
+        const obj = post;
+        const { slug, username, avatar } = obj.author;
+        res.push({ ...obj, author: { slug, username, avatar } });
       }
-      return reply.status(200).send(posts);
+      return reply.status(200).send(res);
     } catch (error) {
       fastify.log.error(error);
       return reply.status(500).send({ msg: 'Something went wrong' });
